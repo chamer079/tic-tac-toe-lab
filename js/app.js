bd44498cf24edb828e5a4f6f3f86faae2de533bb
chamer79 <chamer79@gmail.com>
2024-07-28 18:56:18 -0400

    added pseudocode & completed steps 1-3

--- js/app.js
+++ js/app.js
@@ -1,19 +1,49 @@
 /*-------------------------------- Constants --------------------------------*/
+//5) Define the required constants.
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+//1) Define the required variables used to track the state of the game. -Done
+let borad =
+["", "", "", 
+ "", "", "",  
+ "", "", ""]
+let turn = "x"
+let winner = false
+let tie = false
 
 
 /*------------------------ Cached Element References ------------------------*/
+//2) Store cached element references.
+const squareEls = document.querySelectorAll(".sqr")
+const messageEl = document.querySelector("#message")
 
-
+// console.log(squareEls)
+// console.log(messageEl)
 
 /*-------------------------------- Functions --------------------------------*/
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
 
+//4) The state of the game should be rendered to the user.
 
+function init(){       
+        
+    }
+    console.log("test", init())
 
+function render(){
+    
+}
+    
 /*----------------------------- Event Listeners -----------------------------*/
+//6) Handle a player clicking a square with a `handleClick` function.
+
+//7) Create Reset functionality.
+
+
+
+
 
 
 
